677772095ff87efabcb26e374205f999165a1895
Mike Varano <mikevee313@gmail.com>
2015-11-04 16:40:35 -0500

    added erics stuff

--- src/client/react-gauge.js
+++ src/client/react-gauge.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import ReactGauge from 'react-gauge';
 
 renderGauge();
-document.getElementById('gauge-input').addEventListener('onchange', function() {
+document.getElementById('gauge-input').addEventListener('input', function(e) {
+  let value = e.target.value;
   if (value < 0 || value > 100) return;
   renderGauge(this.value);
 });
